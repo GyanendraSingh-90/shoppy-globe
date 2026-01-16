b18cbbe52414c3cee176686a9ab1c7d2030f4f52
GyanendraSingh-90 <patel90shiva90@gmail.com>
2026-01-16 11:54:15 +0530

    update main.jsx to wrap app with redux prodvider and router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux'; // Redux provider
+import { RouterProvider } from 'react-router-dom'; // React Router
+import store from './redux/store';
+import router from './App'; // Router defined in App.jsx
+import './assets/styles.css'; // Global styles
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
 )
